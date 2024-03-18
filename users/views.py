102b0f655c9930ee590a27870b50418e3c8a397f
TakuroBreath <maxsmurffy@gmail.com>
2024-03-18 13:55:08 +0300

    Services with API work added

--- users/views.py
+++ users/views.py
@@ -1,12 +1,14 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import serializers
 from rest_framework.filters import OrderingFilter
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from users.models import Payment, User
 from users.permissions import IsModerator
 from users.serializers import PaymentSerializer, UserSerializer
+from users.services import create_stripe_session, create_stripe_price
 
 
 class PaymentListAPIView(ListAPIView):
@@ -22,3 +24,19 @@ class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsModerator]
+
+
+class PaymentCreateAPIView(CreateAPIView):
+    serializer_class = PaymentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        course = serializer.validated_data.get('course')
+        if not course:
+            raise serializers.ValidationError('Choose a course')
+
+        payment = serializer.save()
+
+        payment.payment_id, payment.payment_link = create_stripe_session(create_stripe_price(payment))
+
+        payment.save()
